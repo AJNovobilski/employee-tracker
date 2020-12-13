bc7d997375cf26adffac1412f39467d813d4ceca
anovobilskischool <myemail>
2020-12-12 23:05:34 -0800

    master

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE employeeDB;
+
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Marketing'), ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1), ('Salesperson', 80000, 1), ('Lead Engineer', 150000, 2), ('Software Engineer', 120000, 2), ('Accountant', 125000, 3), ('Legal Team Lead', 250000, 4), ('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ('wolf', 'cola', 1, null), ('steve', 'benjamin-Hall', 3, null), ('nu', 'nunu', 4, 2), ('yong', 'mong', 6, null), ('jimmie', 'yuute', 2, 1), ('rita', 'walter', 2, 1);
